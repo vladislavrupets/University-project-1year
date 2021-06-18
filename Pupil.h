5c2c7a14343b22c4e6592c3b5c01bbde672545ef
vladislavrupets <84872808+vladislavrupets@users.noreply.github.com>
2021-06-18 13:00:28 +0300

    Update Pupil.h

--- Pupil.h
+++ Pupil.h
@@ -1,6 +1,16 @@
 #ifndef PUPIL_H
 #define PUPIL_H
 
-class Pupil : public Human{};
+class Pupil : public Human {
+private:
+	std::vector<Eval> evals;
+	std::string _class;
+public:
+	using Human::Human;
+	Pupil(): _class(""), evals(0) {}
+	Pupil(std::string _class0, std::vector<Eval> evals0) : _class(_class0), evals(evals0) {}
+	std::string Info() const;
+	std::vector<Eval> GetEval();
+};
 
 #endif 
