324db6cf79defa76c374cbc72a98da1b03540ee3
vladislavrupets <84872808+vladislavrupets@users.noreply.github.com>
2021-06-18 13:04:42 +0300

    Update Teacher.h

--- Teacher.h
+++ Teacher.h
@@ -1,6 +1,11 @@
 #ifndef TEACHER_H
 #define TEACHER_H
 
-class Teacher : public Human{};
+class Teacher : public Human {
+public:
+        using Human::Human;
+	      Subjects GetSub() const;
+	      std::string Info() const;
+};
 
 #endif 
