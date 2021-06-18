a00689a9c8ad234a4f76f43e725b220806cfdb9e
vladislavrupets <84872808+vladislavrupets@users.noreply.github.com>
2021-06-18 13:06:53 +0300

    Update Eval.h

--- Eval.h
+++ Eval.h
@@ -1,6 +1,17 @@
 #ifndef EVAL_H
 #define EVAL_H
 
-class Eval {};
+class Eval {
+private:
+	enum class Type {
+		HOMEWORK,
+		TEST,
+		INDEPENDENT_WORK,
+		EXAM,
+		FINAL_TEST
+	};
+	std::vector<Subjects>
+public: 
+};
 
 #endif
