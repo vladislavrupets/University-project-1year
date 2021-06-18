067485dc5f9aee0288a5ee4c598fe355788286a7
vladislavrupets <84872808+vladislavrupets@users.noreply.github.com>
2021-06-18 13:04:46 +0300

    Update Teacher.cpp

--- Teacher.cpp
+++ Teacher.cpp
@@ -3,21 +3,9 @@
 #include "Teacher.h"
 #include <string>
 
-enum class Subjects {
-	MATH,
-	ENG,
-	IT,
-	PE,
-	UNKNOWN
-};
-
 class Teacher : public Human {
-private:
-	Subjects subject;
 public:
 	using Human::Human;
-	Teacher() : subject(Subjects::UNKNOWN) {}
-	Teacher(Subjects subject0) : subject(subject0) {}
 	Subjects GetSub() const {
 		return subject;
 	}
